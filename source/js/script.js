a1220d064dda9e0062eb098d33328cab0ff12094
BigGeorgy <98316846+BigGeorgy@users.noreply.github.com>
2022-05-06 21:01:09 +0300

    HW5.9

--- source/js/script.js
+++ source/js/script.js
@@ -20,8 +20,12 @@ navToggle.addEventListener('click', function () {
 
 function fun1() {
   let exampleRange = document.querySelector('.example__block-img-fat');
+  let exampleRangemob = document.querySelector('.example__block-img-fat-mob');
+  let exampleRangemobslider = document.querySelector('.range-mob-thumb');
   let r1 = document.getElementById('myRange');
 
   exampleRange.style.width = r1.value+'px';
+  exampleRangemob.style.width = r1.value/2+'px';
+  exampleRangemobslider.style.width = r1.value/6.5+'px';
 
 };
